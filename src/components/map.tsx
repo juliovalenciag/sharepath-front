9e8b1f8f8716076648e4920b340c621a21984bde
Elyayen <darien13.ariz@gmail.com>
2025-10-26 16:07:57 -0600

    Se agrego los alerts al iniciar sesion y se comenzo con la creacion de itinerario

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,5 +1,9 @@
 import { LatLngExpression } from "leaflet";
+<<<<<<< HEAD
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+=======
 import * as L from "react-leaflet"
+>>>>>>> abf5e16404d6701c2067fcbbef4d463cd7b22909
 import "leaflet/dist/leaflet.css"
 import "leaflet-defaulticon-compatibility"
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css"
@@ -18,6 +22,25 @@ interface MapProps{
 export default function Mapa(props: MapProps) {
   const { posicion, zoom, itinerario, onAddLugar } = props;
 
+<<<<<<< HEAD
+  return (
+    <MapContainer center={posicion} zoom={zoom} scrollWheelZoom={true} className="h-full w-full">
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      <Ruta destinos={itinerario} />
+      <BusquedaMapa onAddLugar={onAddLugar} />
+
+      <Marker position={posicion}>
+        <Popup>
+          ESCOM <br /> Escula Superior de Computo
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+=======
   return <L.MapContainer center={posicion} zoom={zoom} scrollWheelZoom={true} className="h-full w-full">
     <L.TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -34,4 +57,5 @@ export default function Mapa(props: MapProps) {
       </L.Popup>
     </L.Marker>
   </L.MapContainer>
+>>>>>>> abf5e16404d6701c2067fcbbef4d463cd7b22909
 }
