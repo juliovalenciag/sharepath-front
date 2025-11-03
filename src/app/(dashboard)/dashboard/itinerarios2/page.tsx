b8a2fda84f2f82ff2f9e1a59e92a8917cd01aeb1
HarolBarajas <fabianbap32@gmail.com>
2025-11-02 22:37:18 -0600

    Implementación de los componentes para ver itinerarios

---
+++ src/app/(dashboard)/dashboard/itinerarios2/page.tsx
@@ -0,0 +1,39 @@
+import ItinerarioFrame from "@/components/dashboard-components/ItinerarioFrame";
+const datosPublicacion = {
+  id: "t1",
+  tituloPrincipal: "Tepoztlan, Morelos",
+  subtitulo: "Pueblo Mágico de Tepoztlan",
+  calificacion: 4.5,
+  fechaInicio: "2024-08-01",
+  detallesLugar: "Este lugar es conocido por su rica cultura y hermosos paisajes.",
+  dias: [
+    { id: 1, dia: "Día 1", categoria: "Hotel", titulo: "Posada del Tepozteco", urlImagen: "/img/tepozteco1.jpg", calificacion:"3.2"},
+    { id: 2, dia: "Día 2", categoria: "Cultura", titulo: "Cerro del Tepozteco", urlImagen: "/img/tepozteco2.jpg", calificacion:"2.2"},
+    { id: 3, dia: "Día 3", categoria: "Comida", titulo: "Mercado de Tepoztlán", urlImagen: "/img/tepozteco3.jpg", calificacion:"5.0"},
+    { id: 4, dia: "Día 4", categoria: "Descanso", titulo: "Balneario de Atongo", urlImagen: "/img/tepozteco4.jpg", calificacion:"2.4" },
+  ],
+};
+
+export default function PublicacionPage() {
+  return (
+    <>    <div className="p-10">
+      <ItinerarioFrame itinerario={datosPublicacion} />
+    </div>
+    <div className="p-10">
+      <ItinerarioFrame itinerario={datosPublicacion} />
+    </div>
+    <div className="p-10">
+      <ItinerarioFrame itinerario={datosPublicacion} />
+    </div>
+    <div className="p-10">
+      <ItinerarioFrame itinerario={datosPublicacion} />
+    </div>
+    <div className="p-10">
+      <ItinerarioFrame itinerario={datosPublicacion} />
+    </div>
+    <div className="p-10">
+      <ItinerarioFrame itinerario={datosPublicacion} />
+    </div>
+</>
+  );
+}
---
+++ src/components/dashboard-components/ItinerarioFrame.tsx
@@ -0,0 +1,158 @@
+// components/ItinerarioFrame.tsx
+"use client";
+import DiasCarousel from "./DiaCarousel";
+import Link from "next/link";
+import { useState } from "react"
+import { MoreHorizontalIcon } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Field, FieldGroup, FieldLabel } from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Textarea } from "@/components/ui/textarea"
+interface ItinerarioFrameProps {
+  itinerario: ItinerarioPrincipal; // Recibe la estructura completa
+}
+import { Star } from "lucide-react";
+
+function Estrellas({ calificacion }: { calificacion: number }) {
+  const totalEstrellas = 5;
+  const estrellasLlenas = Math.floor(calificacion);
+  const tieneMedia = calificacion - estrellasLlenas >= 0.5;
+
+  return (
+    <div className="flex gap-1 text-yellow-400">
+      {Array.from({ length: totalEstrellas }).map((_, i) => {
+        if (i < estrellasLlenas) {
+          return <Star key={i} fill="currentColor" stroke="currentColor" />;
+        } else if (i === estrellasLlenas && tieneMedia) {
+          return <Star key={i} fill="currentColor" strokeWidth={1.5} />;
+        } else {
+          return <Star key={i} stroke="currentColor" />;
+        }
+      })}
+    </div>
+  );
+}
+const ItinerarioFrame: React.FC<ItinerarioFrameProps> = ({ itinerario }) => {
+    const [showNewDialog, setShowNewDialog] = useState(false)
+    const [showShareDialog, setShowShareDialog] = useState(false)
+  return (
+    <>
+        <div className="bg-gray-900 text-white p-6 flex rounded-lg min-h-[300px] w-full">
+            <DropdownMenu modal={false}>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" aria-label="Open menu" size="icon-sm">
+            <MoreHorizontalIcon />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-40" align="end">
+          <DropdownMenuLabel>Editar</DropdownMenuLabel>
+          <DropdownMenuGroup>
+            <DropdownMenuItem onSelect={() => setShowNewDialog(true)}>
+              <p className="">Eliminar</p>
+            </DropdownMenuItem>
+            <DropdownMenuItem disabled>Descargar</DropdownMenuItem>
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <Dialog open={showNewDialog} onOpenChange={setShowNewDialog}>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Create New File</DialogTitle>
+            <DialogDescription>
+              Provide a name for your new file. Click create when you&apos;re
+              done.
+            </DialogDescription>
+          </DialogHeader>
+          <FieldGroup className="pb-3">
+            <Field>
+              <FieldLabel htmlFor="filename">File Name</FieldLabel>
+              <Input id="filename" name="filename" placeholder="document.txt" />
+            </Field>
+          </FieldGroup>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DialogClose>
+            <Button type="submit">Create</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+      <Dialog open={showShareDialog} onOpenChange={setShowShareDialog}>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Share File</DialogTitle>
+            <DialogDescription>
+              Anyone with the link will be able to view this file.
+            </DialogDescription>
+          </DialogHeader>
+          <FieldGroup className="py-3">
+            <Field>
+              <Label htmlFor="email">Email Address</Label>
+              <Input
+                id="email"
+                name="email"
+                type="email"
+                placeholder="shadcn@vercel.com"
+                autoComplete="off"
+              />
+            </Field>
+            <Field>
+              <FieldLabel htmlFor="message">Message (Optional)</FieldLabel>
+              <Textarea
+                id="message"
+                name="message"
+                placeholder="Check out this file"
+              />
+            </Field>
+          </FieldGroup>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DialogClose>
+            <Button type="submit">Send Invite</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+          <div className="w-1/3 p-4 flex flex-col justify-center">
+            <h1 className="text-3xl font-bold text-blue-400 mb-2">
+              {itinerario.tituloPrincipal}
+            </h1>
+            <h2 className="text-xl font-semibold mb-6">
+              {itinerario.subtitulo}
+            </h2>
+            <p className="text-blue-400 mb-2">
+              Fecha de inicio {itinerario.fechaInicio}
+            </p>
+            <p className="text-sm">
+              Detalles del lugar: {itinerario.detallesLugar}
+            </p>
+          </div>
+
+          <div className="w-2/3 flex items-center">
+            <DiasCarousel dias={itinerario.dias} />
+          </div>
+        </div>
+
+    </>
+  );
+};
+
+export default ItinerarioFrame;
