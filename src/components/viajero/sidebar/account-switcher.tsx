8186d4b81c09d32b4b77ad8dff3e3ecd5ea4febf
HarolBarajas <fabianbap32@gmail.com>
2025-12-08 18:20:07 -0600

    Cambio de rutas a railway

--- src/components/viajero/sidebar/account-switcher.tsx
+++ src/components/viajero/sidebar/account-switcher.tsx
@@ -61,8 +61,8 @@ export function AccountSwitcher({ type = "viajero" }: AccountSwitcherProps) {
             ? localStorage.getItem("authToken")
             : null;
 
-        //const res = await fetch("https://harol-lovers.up.railway.app/user", {
-           const res = await fetch("http://localhost:4000/user", {
+        const res = await fetch("https://harol-lovers.up.railway.app/user", {
+          //const res = await fetch("http://localhost:4000/user", {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
